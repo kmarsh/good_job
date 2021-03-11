05743f68d10a075c56626f511f7d1ffc86be6701
Kevin Marsh <kevin.marsh@gmail.com>
2021-03-11 12:32:59 -0500

    Adds Bootstrap flash helpers

--- engine/app/helpers/good_job/application_helper.rb
+++ engine/app/helpers/good_job/application_helper.rb
@@ -1,4 +1,17 @@
 module GoodJob
   module ApplicationHelper
+    def flash_class(key)
+      rails_to_bootstrap = {
+        alert: :warning,
+        error: :danger,
+        notice: :info,
+        success: :success,
+      }
+
+      rails_class = key
+      bootstrap_class = rails_to_bootstrap[key]
+
+      "flash flash-#{rails_class} alert alert-#{bootstrap_class}"
+    end
   end
 end
